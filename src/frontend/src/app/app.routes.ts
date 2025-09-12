88fd4963c68c86996716d634cf587e74dd0b2bc6
Garth Groshans Jr <garthgroshansjr@gmail.com>
2025-09-12 14:50:56 -0400

    fe configuration

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Stories } from './src/pages/stories/stories';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Stories, data: { category: 'top' }, title: 'Hacker News' },
+  { path: 'new', component: Stories, data: { category: 'new' }, title: 'New Stories' },
+  { path: 'best', component: Stories, data: { category: 'best' }, title: 'Best Stories' },
+  { path: 'ask', component: Stories, data: { category: 'ask' }, title: 'Ask HN' },
+  { path: 'show', component: Stories, data: { category: 'show' }, title: 'Show HN' },
+  { path: 'jobs', component: Stories, data: { category: 'job' }, title: 'Jobs' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs';
+import { Header } from '../app/src/components/header/header';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private titleService = inject(Title);
+
+  constructor() {
+    this.router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+        map(() => {
+          let route = this.activatedRoute;
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary'),
+        mergeMap((route) => route.data)
+      )
+      .subscribe((event) => this.titleService.setTitle(event['title'] || 'Hacker News'));
+  }
 }
