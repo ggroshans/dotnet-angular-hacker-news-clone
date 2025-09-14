01dd9b4f4ff19ba6b35a759e94bcb81b1c8e72b3
Garth Groshans Jr <garthgroshansjr@gmail.com>
2025-09-14 09:52:17 -0400

    create 'url' and 'timestamp' formatting for story list item => domain & timeAgo pipes

---
+++ src/frontend/src/app/pipes/domain.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'domain',
+  standalone: true,
+})
+export class DomainPipe implements PipeTransform {
+  transform(url: string | undefined): string {
+    if (!url) {
+      return '';
+    }
+    try {
+      const hostname = new URL(url).hostname;
+      return `(${hostname.replace(/^www\./, '')})`;
+    } catch (e) {
+      return '';
+    }
+  }
+}
